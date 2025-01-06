5f793d229692cc423f45c65886da3ad5e76e9528
gehsso <35506437+gehsso@users.noreply.github.com>
2025-01-06 11:36:20 -0300

    Create testeoo01.py

---
+++ 10/testeoo01.py
@@ -0,0 +1,35 @@
+print("Olá, mund, testanto o python!")
+
+
+# Variáveis e tipos básicos
+nome = "Maria"
+idade = 25
+altura = 1.68
+esta_estudando = True
+
+print(nome, idade, altura, esta_estudando)
+
+
+
+nome = input("Digite seu nome: ")
+print("Olá,", nome, "seja bem-vindo!")
+
+
+
+
+# Soma de dois números
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+soma = num1 + num2
+print("A soma é:", soma)
+
+    
+
+
+numero = int(input("Digite um número: "))
+
+if numero % 2 == 0:
+    print("O número é par.")
+else:
+    print("O número é ímpar.")
